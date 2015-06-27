fe9a36cad4b0fd15d50424eacf91fa990631a664
Declan McKelvey-Hembree <djmckelv@wustl.edu>
2015-06-27 16:17:47 -0500

    primitive Collision Checking

--- Assets/PlanePilot.cs
+++ Assets/PlanePilot.cs
@@ -10,6 +10,9 @@ public class PlanePilot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Physics.CheckSphere(transform.position+(transform.forward*2.0f),0.0f)){
+			return;
+		}
 		Vector3 moveCamTo = transform.position - transform.forward *10.0f + Vector3.up*5.0f;
 		float bias=0.96f;
 		Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo*(1.0f-bias);
