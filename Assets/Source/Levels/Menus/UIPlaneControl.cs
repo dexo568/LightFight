4767f35976d5dcc1a5afc30a5cc7b86083dd55bd
Declan McKelvey-Hembree <djmckelv@wustl.edu>
2015-10-02 01:51:28 -0500

    Shitty looking main menu

---
+++ Assets/Source/Levels/Menus/UIPlaneControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIPlaneControl : MonoBehaviour {
+	private Vector3 restLocation;
+	// Use this for initialization
+	void Start () {
+		restLocation = transform.position;
+		transform.position = transform.position - transform.forward * 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((transform.position - restLocation).magnitude > 5){
+			transform.position += transform.forward;
+		}
+	}
+	public void continueMoving(){
+		restLocation = new Vector3(999f,999f,999f);
+	}
+}
