e11f0e9c2d7dc47e191bf40c087cfb2aa3e81bc1
Jordan Mecom <jmecom@wustl.edu>
2015-10-02 11:08:33 -0500

    add some networking

---
+++ Assets/Source/Levels/Menus/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+	void Start () {}
+	void Update () {}
+
+	public void BeginLocalGame() {
+		Application.LoadLevel("canyonracescene");
+	}
+
+	public void BeginNetworkGame() {
+		Application.LoadLevel("OnlineLobbyScene");
+	}
+
+}
